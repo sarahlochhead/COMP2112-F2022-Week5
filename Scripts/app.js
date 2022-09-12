483fbf703b3ccbb0159b8b4fdc58c2808b09dac6
Sarah Lochhead <sarahlochhead@gmail.com>
2022-09-12 10:55:44 -0400

    added third party libraries

---
+++ Scripts/app.js
@@ -0,0 +1,13 @@
+// IIFE -- Immediately Invoked Function Expression
+// AKA - Self Executing Function
+(function() 
+{
+    
+    function Start()
+    {
+        console.log("App Started!");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
