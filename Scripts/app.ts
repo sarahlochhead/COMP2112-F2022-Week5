21c0f3caabf00de8b4d5e00f2244e44ab0e0dfb6
Sarah Lochhead <sarahlochhead@gmail.com>
2022-09-19 11:03:44 -0400

    transcompiled ts to js

--- Scripts/app.ts
+++ Scripts/app.ts
@@ -1,39 +1,53 @@
 "use strict";
 // IIFE -- Immediately Invoked Function Expression
 // AKA - Self Executing Function
-(function () {
+(function() 
+{
     /**
      * This function loads data Asynchronously from a URL.
      * It calls the callback function when the data loading is complete
-     *
-     * @param {string} method
-     * @param {string} url
-     * @param {function} callback
+     * 
+     * @param {string} method 
+     * @param {string} url 
+     * @param {function} callback 
      */
-    function LoadData(method, url, callback) {
+    function LoadData(method: string, url: string, callback: Function): void 
+    {
         // Step 1 - Create an empty XHR object
         let XHR = new XMLHttpRequest();
+
         // Step 2 - Compose the Request
         XHR.open(method, url);
+
         // Step 3 - Send the Request to the server
         XHR.send();
+
         // Step 4 - Setup and event listener
-        XHR.addEventListener("readystatechange", function () {
-            if ((XHR.status == 200) && (XHR.readyState == 4)) {
+        XHR.addEventListener("readystatechange", function(){
+
+            if((XHR.status == 200) && (XHR.readyState == 4))
+            {
                 callback(XHR.responseText);
             }
+
         });
     }
+
     // First method of using functions - a named function
-    function Start() {
+    function Start()
+    {
         console.log("App Started!");
+
         // LoadData("GET", "./Data/contacts.json", function(XHR){
         //     console.log(XHR);
         // });
-        $.getJSON("./Data/contacts.json", function (DataSource) {
+
+        $.getJSON("./Data/contacts.json", function(DataSource){
             console.log(DataSource.ContactList[0]);
         });
+
     }
+
     window.addEventListener("load", Start);
+    
 })();
-//# sourceMappingURL=app.js.map
