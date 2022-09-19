33b8d9640e4330901171c684570deb7cb2632dcf
Sarah Lochhead <sarahlochhead@gmail.com>
2022-09-19 10:36:22 -0400

    added ContactList datasource

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,13 +1,51 @@
+"use strict";
 // IIFE -- Immediately Invoked Function Expression
 // AKA - Self Executing Function
 (function() 
 {
-    
+    /**
+     * This function loads data Asynchronously from a URL.
+     * It calls the callback function when the data loading is complete
+     * 
+     * @param {string} method 
+     * @param {string} url 
+     * @param {function} callback 
+     */
+    function LoadData(method, url, callback) 
+    {
+        // Step 1 - Create an empty XHR object
+        let XHR = new XMLHttpRequest();
+
+        // Step 2 - Compose the Request
+        XHR.open(method, url);
+
+        // Step 3 - Send the Request to the server
+        XHR.send();
+
+        // Step 4 - Setup and event listener
+        XHR.addEventListener("readystatechange", function(){
+
+            if((XHR.status == 200) && (XHR.readyState == 4))
+            {
+                callback(XHR.responseText);
+            }
+
+        });
+    }
+
+    // First method of using functions - a named function
     function Start()
     {
         console.log("App Started!");
+
+        LoadData("GET", "./Data/contacts.json", function(XHR){
+            console.log(XHR);
+        });
+
     }
 
-    window.addEventListener("load", Start);
+    
 
+    window.addEventListener("load", Start);
+    
 })();
