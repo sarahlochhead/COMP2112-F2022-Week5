9aafeb27e30fd10207c7ae6de10f4575406df718
Sarah Lochhead <sarahlochhead@gmail.com>
2022-09-26 15:21:35 -0400

    updated project files

--- Scripts/app.ts
+++ Scripts/app.ts
@@ -3,45 +3,11 @@
 // AKA - Self Executing Function
 (function() 
 {
-    /**
-     * This function loads data Asynchronously from a URL.
-     * It calls the callback function when the data loading is complete
-     * 
-     * @param {string} method 
-     * @param {string} url 
-     * @param {function} callback 
-     */
-    function LoadData(method: string, url: string, callback: Function): void 
-    {
-        // Step 1 - Create an empty XHR object
-        let XHR = new XMLHttpRequest();
-
-        // Step 2 - Compose the Request
-        XHR.open(method, url);
-
-        // Step 3 - Send the Request to the server
-        XHR.send();
-
-        // Step 4 - Setup and event listener
-        XHR.addEventListener("readystatechange", function(){
-
-            if((XHR.status == 200) && (XHR.readyState == 4))
-            {
-                callback(XHR.responseText);
-            }
-
-        });
-    }
-
     // First method of using functions - a named function
     function Start()
     {
         console.log("App Started!");
-
-        // LoadData("GET", "./Data/contacts.json", function(XHR){
-        //     console.log(XHR);
-        // });
-
+        
         $.getJSON("./Data/contacts.json", function(DataSource){
             console.log(DataSource.ContactList[0]);
         });
